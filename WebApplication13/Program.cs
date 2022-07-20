98771ee8c325a832d7f6de21f90bd517963c6ad5
Chen An <v-chenan1@microsoft.com>
2022-07-20 08:03:30 +0000

    2

--- WebApplication13/Program.cs
+++ WebApplication13/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-//1
+//12
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
